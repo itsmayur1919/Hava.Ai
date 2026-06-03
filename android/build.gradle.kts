8f4633d303ef254eeff6027c50856149560572b7
Mayur Shinde <harshmayur1704@gmail.com>
2026-06-03 19:59:19 +0530

    Fix the Material solution

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,4 +1,8 @@
 plugins {
     id("com.android.application") version "8.1.1" apply false
     kotlin("android") version "1.9.10" apply false
+    
+}
+dependencies {
+  implementation("com.google.android.material:material:1.11.0")
 }
