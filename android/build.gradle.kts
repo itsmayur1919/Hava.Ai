901630b98ed6f2f87e63528169ff334d913bc2c3
Mayur Shinde <harshmayur1704@gmail.com>
2026-06-03 18:07:31 +0530

    Fix the gradle wrapper version & kts file

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -2,12 +2,3 @@ plugins {
     id("com.android.application") version "8.1.1" apply false
     kotlin("android") version "1.9.10" apply false
 }
-
-dependencyResolutionManagement {
-  repositories {
-    google()
-    mavenCentral()
-  }
-}
-
-rootProject.name = "havaman-android"
